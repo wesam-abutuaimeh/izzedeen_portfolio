1bfb88a04c4557728d477b0f6b6079ec8b4e168d
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2024-11-06 13:56:56 +0200

    V01: This is the first version of the website, featuring basic form functionality and API integration.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import LandingSection from "./pages/LandingSection";
+import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
+import { NotFound } from "./pages/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainContainer>
+        <Routes>
+          <Route index element={<LandingSection />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MainContainer>
     </div>
   );
 }
